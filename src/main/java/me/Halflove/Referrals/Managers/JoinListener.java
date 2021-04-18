07a02b4723457047159d591505e2d2947794ab6a
BuildTools <unconfigured@null.spigotmc.org>
2021-04-17 20:06:40 -0600

    Updated progress

---
+++ src/main/java/me/Halflove/Referrals/Managers/JoinListener.java
@@ -0,0 +1,16 @@
+package me.Halflove.Referrals.Managers;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class JoinListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event){
+        Player player = event.getPlayer();
+        DatabaseManager.updateReferrals(player.getUniqueId(), 0);
+    }
+
+}
