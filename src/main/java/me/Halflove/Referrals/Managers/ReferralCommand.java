2c2512be3dd8ad3fa95fcd2ef26bb0139a1d68ef
BuildTools <unconfigured@null.spigotmc.org>
2021-04-17 18:54:36 -0600

    Added some catches

--- src/main/java/me/Halflove/Referrals/Managers/ReferralCommand.java
+++ src/main/java/me/Halflove/Referrals/Managers/ReferralCommand.java
@@ -10,17 +10,25 @@ public class ReferralCommand implements CommandExecutor {
 
     public boolean onCommand(CommandSender sender, Command cmd, String lavel, String[] args){
         if(cmd.getName().equalsIgnoreCase("referrals")){
-            if(sender instanceof Player)
+            if(!(sender instanceof Player))
                 return true;
 
             Player player = (Player) sender;
             switch(args.length){
                 default:
-                    referralsMenu(player);
+                    ReferralGUI.openGui(player);
                     break;
                 case 1:
                     if(Bukkit.getPlayer(args[0]) != null){
-                        ReferralManager.confirmInvitation(player,Bukkit.getPlayer(args[0]));
+                        if(!player.hasPlayedBefore()){
+                            if(!args[0].equals(player.getName())){
+                                ReferralManager.confirmInvitation(player,Bukkit.getPlayer(args[0]));
+                            }else{
+                                player.sendMessage("You can't invite yourself.");
+                            }
+                        }else{
+                            player.sendMessage("You can't do this, you've played before.");
+                        }
                     }else{
                         player.sendMessage("Player not online");
                     }
@@ -28,10 +36,6 @@ public class ReferralCommand implements CommandExecutor {
             }
         }
         return true;
-    }
-
-    public static void referralsMenu(Player player){
-
     }
 
 }
