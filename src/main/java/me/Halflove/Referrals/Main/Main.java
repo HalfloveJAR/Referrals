8ab8f01bf77240181765f3fb28169fa4a85b6615
BuildTools <unconfigured@null.spigotmc.org>
2021-04-17 18:54:10 -0600

    Register events and register commands

--- src/main/java/me/Halflove/Referrals/Main/Main.java
+++ src/main/java/me/Halflove/Referrals/Main/Main.java
@@ -1,18 +1,31 @@
 package me.Halflove.Referrals.Main;
 
 import me.Halflove.Referrals.Managers.DatabaseManager;
+import me.Halflove.Referrals.Managers.ReferralCommand;
+import me.Halflove.Referrals.Managers.ReferralGUI;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.sql.Connection;
 
 public class Main extends JavaPlugin {
 
     public DatabaseManager database = DatabaseManager.instance;
 
     public void onEnable(){
-        database.setupLocalDatabase((Plugin) this);
         //database.setupSQLDatabase();
+        database.setupLocalDatabase((Plugin) this);
+        registerCommands();
+        registerEvents();
+    }
+
+    public void registerEvents(){
+        Bukkit.getPluginManager().registerEvents((Listener) new ReferralGUI(), (Plugin) this);
+    }
+
+    public void registerCommands(){
+        getCommand("referrals").setExecutor((CommandExecutor) new ReferralCommand());
     }
 
 }
